8b74c4ee72aeca6b66323fc89d6f3f0fcf0b3f01
nicsinha <sinhanikhil097@gmail.com>
2024-01-14 14:21:02 +0530

    User upload file and we fetched into our project

--- url-shortener/app.py
+++ url-shortener/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 #This secret_key allow us to securely send messages bach and forth from the user make sure that thore trying to
@@ -28,8 +28,16 @@ def your_url():
         if request.form['code'] in urls.keys():
             flash('That short name has already been taken. Please select another name')
             return redirect(url_for('home'))
+        
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('D:\\Git\\Flask-Project\\url-shortener\\' + full_name)
+            urls[request.form['code']] = {'file':full_name}
 
-        urls[request.form['code']] = {'url':request.form['url']}
+        
         with open('urls.json','w') as url_file:
             json.dump(urls,url_file)
     
