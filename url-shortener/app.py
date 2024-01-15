563ce41d31669e49260d6b84c79a61b58625699a
nicsinha <sinhanikhil097@gmail.com>
2024-01-15 21:17:49 +0530

    Added session and cookies

--- url-shortener/app.py
+++ url-shortener/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -14,7 +14,7 @@ app.secret_key = "asdertgcde1279"
 def home():
     #return "Hello Flask !!"
     #here name is jinja var.
-    return render_template('home.html')
+    return render_template('home.html', codes = session.keys())
 
 @app.route('/your-url',methods=['GET','POST'])
 def your_url():
@@ -42,6 +42,7 @@ def your_url():
         
         with open('urls.json','w') as url_file:
             json.dump(urls,url_file)
+            session[request.form['code']] = True
     
     
         return render_template('your-url.html',code = request.form['code'])
